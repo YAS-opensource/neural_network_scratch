3395158cf6bfa13579ab13ee7d85a1d086d796a8
SedativeHypnotics <sihantawsik@gmail.com>
2020-03-16 00:29:32 +0600

    created module

---
+++ main.py
@@ -0,0 +1,5 @@
+from model.model import Model
+
+if __name__ == ("__main__"):
+    model = Model()
+    print(model.test())
---
+++ model/model.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class Model(object):
+    def __init__(self):
+        pass
