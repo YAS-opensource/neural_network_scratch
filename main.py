70b42844b567cd1cdb4db33213ad49f2e8c86a5a
SedativeHypnotics <sihantawsik@gmail.com>
2020-03-16 01:38:26 +0600

    added model function derivative, sigmoid, add_layer and train

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from model.model import Model
 
 if __name__ == ("__main__"):
     model = Model()
+    model.add_layer(3, model.sigmoid)
+    model.train([[1], [2], [3]])
--- model/model.py
+++ model/model.py
@@ -1,5 +1,29 @@
 import numpy as np
+from math import exp
 
 class Model(object):
     def __init__(self):
+        self.layers = []
+        self.bias = []
+
+    def sigmoid(self, x):
+        return 1/(1+exp(-x))
+
+    def softmax(self):
         pass
+
+    def _differentiation(self, func, x):
+        if func == self.sigmoid:
+            return exp(x)/(1+exp(x))**2
+
+    def add_layer(self, neurons, func):
+        layer = np.random.rand(neurons, 1)
+        bias = np.random.rand(neurons, 1)
+        self.layers.append((layer, func))
+        self.bias.append(bias)
+
+    def train(self, data, epoch=10, learn_rate=0.001, batch_size=2):
+        data = np.array(data, dtype=np.float32)
+        if data.shape != self.layers[0][0].shape:
+            raise ValueError("Shape does not match")
+        self.layers[0] = (np.copy(data), self.layers[0][1])
