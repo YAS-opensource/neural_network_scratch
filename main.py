a56ff4e3b570dc287d76de83e094431307ef0e28
SedativeHypnotics <sihantawsik@gmail.com>
2020-03-17 20:15:19 +0600

    implemented batch forward feed

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from model.model import Model
+import numpy as np
 
 if __name__ == ("__main__"):
     model = Model()
+    data = [[1, 2, 7], [4, 5, 9], [3, 8, 6]]
+    label = [0, 1, 3]
+    data = np.array(data, dtype=np.float32)
+    # mean = data.mean(axis=0)
+    # std = data.std(axis=0)
+    # data = data - mean
+    # data = data/std
+    print(data)
     model.add_layer(3, model.sigmoid)
-    model.train([[1], [2], [3]])
+    model.add_layer(4, model.sigmoid)
+    model.train(data, label, 1, 0.001, 2)
+    # print(model.layers)
--- model/model.py
+++ model/model.py
@@ -1,29 +1,72 @@
 import numpy as np
 from math import exp
 
+
 class Model(object):
     def __init__(self):
         self.layers = []
         self.bias = []
+        self.weights = []
 
     def sigmoid(self, x):
-        return 1/(1+exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     def softmax(self):
         pass
 
     def _differentiation(self, func, x):
         if func == self.sigmoid:
-            return exp(x)/(1+exp(x))**2
+            return x * (1 - x)
 
     def add_layer(self, neurons, func):
         layer = np.random.rand(neurons, 1)
         bias = np.random.rand(neurons, 1)
+        bias_size = len(self.bias)
+        if bias_size > 0:
+            m = self.bias[bias_size - 1].shape[0]
+            weight = np.random.rand(neurons, m)
+            self.weights.append(weight)
         self.layers.append([layer, func])
         self.bias.append(bias)
 
-    def train(self, data, epoch=10, learn_rate=0.001, batch_size=2):
-        data = np.array(data, dtype=np.float32)
-        if data.shape != self.layers[0][0].shape:
-            raise ValueError("Shape does not match")
-        self.layers[0][0] = np.copy(data)
+    def _error_calculation(self, data, label):
+        total = data-label
+        total = total.sum(axis=1)
+        # total = total/total.shape[0]
+        return total
+
+    def _forward_feed(self, data, label, batch_size):
+        steps = data.shape[1] // batch_size + 1
+        error = 0
+        for i in range(steps):
+            train_layers = []
+            data_matrix = data[
+                np.ix_(
+                    list(range(data.shape[0])),
+                    list(
+                        range(i * batch_size, min((i + 1) * batch_size, data.shape[1]))
+                    ),
+                )
+            ]
+            if len(data_matrix) > 0:
+                layers = len(self.bias)
+                data_len = data_matrix.shape[1]
+                current_layer = data_matrix
+                train_layers.append(data_matrix)
+                for j in range(layers-1):
+                    hidden_layer = self.layers[j][1](np.dot(self.weights[j], current_layer)+np.tile(self.bias[j+1], data_len))
+                    train_layers.append(hidden_layer)
+                    current_layer = hidden_layer
+                result = train_layers[-1]
+                y = np.zeros(result.shape)
+                rows = label[i * batch_size: min((i + 1) * batch_size, data.shape[1])]
+                y[rows, list(range(result.shape[1]))] = 1.0
+                error = self._error_calculation(result, y)
+                raw_error = np.dot(np.transpose(error), error)
+                print(raw_error)
+
+    def train(self, data, label, epoch=10, learn_rate=0.001, batch_size=2):
+        data = np.transpose(data)
+
+        for i in range(epoch):
+            self._forward_feed(data, label, batch_size)
